106e0ba14306ceba1162114281d961b572abe6a9
2021110765@dgu.ac.kr <2021110765@dgu.ac.kr>
2024-07-26 23:36:07 +0900

    test

--- src/router.jsx
+++ src/router.jsx
@@ -1,23 +1,25 @@
 // Router.jsx
 
-import { createBrowserRouter } from 'react-router-dom';
-import App from './App';
-import Splash from './pages/splashPage/Splash';
-import Login from './pages/loginPage/Login';
-import Home from './pages/homePage/Home';
-import NotFound from './pages/NotFound/NotFound';
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Splash from "./pages/splashPage/Splash";
+import Login from "./pages/loginPage/Login";
+import Home from "./pages/homePage/Home";
+import NotFound from "./pages/NotFound/NotFound";
+import MyPage from "./pages/myPage/MyPage";
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-        children: [
-        // 메인페이지
-        { path: '/', element: <Splash /> }, // 스플래시 화면
-        { path: '/login', element: <Login /> }, // 로그인 화면
-        { path: '/home', element: <Home /> }, // 홈 화면
-        ],
-        errorElement: <NotFound />,
-    },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      // 메인페이지
+      { path: "/", element: <Splash /> }, // 스플래시 화면
+      { path: "/login", element: <Login /> }, // 로그인 화면
+      { path: "/home", element: <Home /> }, // 홈 화면
+      { path: "/mypage", element: <MyPage /> }, //마이페이지 화면
+    ],
+    errorElement: <NotFound />,
+  },
 ]);
 
 export default router;
