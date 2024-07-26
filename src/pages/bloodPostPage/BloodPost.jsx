57fab263146e2f7e58c24ad335d3d4b510d7b1bf
nicerjs23 <nicerjs23@gmail.com>
2024-07-26 23:16:31 +0900

    ✨ Feat: 긴급헌혈페이지 추가

---
+++ src/pages/bloodPostPage/BloodPost.jsx
@@ -0,0 +1,18 @@
+import {
+    Wrapper,
+} from './Styled';
+import Header from './header/Header';
+const BloodPost = () => {
+    return (
+    <>
+    <Header /> 
+    {/* 이런식으로 헤더를 각페이지에서 따로 임포트시키는게 나을듯요  */}
+    <Wrapper>
+        <div>긴급헌혈페이지</div>
+    </Wrapper>
+    </>
+    )
+};
+
+export default BloodPost;
+    
---
+++ src/pages/bloodPostPage/Styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    display  :flex ;
+    justify-content: center;
+    width: 100%;
+    height: 80vh;
+    font-size: 20px;
+    font-family: SUIT;//폰트사용 예시 
+    font-weight: 800;
+
+`;
+
