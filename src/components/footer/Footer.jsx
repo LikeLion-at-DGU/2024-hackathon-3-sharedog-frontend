9eac226c6a5f753d7783e1d7c5d54db68c4e325d
nicerjs23 <nicerjs23@gmail.com>
2024-07-27 18:34:49 +0900

    💄 Design: 푸터 디자인 변경

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,15 +5,18 @@ import HomeIcon  from '../../assets/icons/footerHome.svg?react';
 import ReserveIcon  from '../../assets/icons/footerReserve.svg?react';
 import MyPageIcon  from '../../assets/icons/footerMyPage.svg?react';
 import BloodIcon  from '../../assets/icons/footerBloodPost.svg?react';
-
+import FillHome from '../../assets/icons/fillHome.svg?react'
+import FillReserve from '../../assets/icons/fillReserve.svg?react'
+import FillMyPage from '../../assets/icons/fillMyPage.svg?react'
+import FillBlood from '../../assets/icons/fillBloodPost.svg?react'
 const Footer = () => {
   return (
     <S.Wrapper>
         <S.IconWrapper>
-            <S.FooterIcon Svg={HomeIcon} label="홈" to="/home" />
-            <S.FooterIcon Svg={ReserveIcon} label="헌혈예약" to="/reservation" />
-            <S.FooterIcon Svg={BloodIcon} label="긴급헌혈" to="/bloodPost" />
-            <S.FooterIcon Svg={MyPageIcon} label="마이페이지" to="/mypage" />
+            <S.FooterIcon Svg={location.pathname === '/home' ? FillHome : HomeIcon} label="홈" to="/home" />
+            <S.FooterIcon Svg={location.pathname === '/reservation' ? FillReserve : ReserveIcon} label="헌혈예약" to="/reservation" />
+            <S.FooterIcon Svg={location.pathname === '/bloodPost' ? FillBlood : BloodIcon} label="긴급헌혈" to="/bloodPost" />
+            <S.FooterIcon Svg={location.pathname === '/mypage' ? FillMyPage : MyPageIcon} label="마이페이지" to="/mypage" />
         </S.IconWrapper>
     </S.Wrapper>
 );
