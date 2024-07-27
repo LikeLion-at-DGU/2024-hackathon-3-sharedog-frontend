5552500f270068eb37233136815d28618c36021c
nicerjs23 <nicerjs23@gmail.com>
2024-07-27 19:46:02 +0900

    🐛 Fix: footer 경고메세지 뜨는거 수정

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,5 +1,5 @@
 import {
-    Wrapper,
+    Wrapper,Bg,
 } from './Styled';
 import Header from './header/Header';
 import React from 'react';
@@ -9,9 +9,11 @@ const Home = () => {
     <>
     <Header /> 
     {/* 이런식으로 헤더를 각페이지에서 따로 임포트시키는게 나을듯요  */}
-    <Wrapper>
-        <div>홈화면</div>
-    </Wrapper>
+    <Bg>
+        <Wrapper>
+            <div>홈화면</div>
+        </Wrapper>
+    </Bg>
     </>
 
   );
--- src/pages/homePage/Styled.jsx
+++ src/pages/homePage/Styled.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import BgImg from '../../assets/images/HomeBack.png'
 
 export const Wrapper = styled.div`
     display  :flex ;
@@ -6,8 +7,17 @@ export const Wrapper = styled.div`
     width: 100%;
     height: 80vh;
     font-size: 20px;
-    font-family: SUIT;//폰트사용 예시 
+    font-family: SUIT, sans-serif;//폰트사용 예시 
     font-weight: 800;
 
 `;
 
+export const Bg = styled.div`
+    background: url(${BgImg}) no-repeat center center;
+    background-size: contain;
+    width: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
