c5f5e2a3bfc7a8410386413311d0b9d8235673a8
nicerjs23 <nicerjs23@gmail.com>
2024-07-28 10:45:45 +0900

    Fix: 깃허브 폴더연동문제 Icons

---
+++ src/data/dummyBloodPost.jsx
@@ -0,0 +1,27 @@
+
+const dummyBloodPost =[
+    {
+        "id": 1,
+        "title": "제목_2",
+        "created_at": "2024-07-26T18:13:33.128821",
+        "updated_at": "2024-07-26T21:16:43.449809",
+        "image": null,
+        "comments_cnt": 2,
+        "like_num": 1,
+        "blood": "DEA 1-",
+        "region": "서울"
+    },
+    {
+        "id": 2,
+        "title": "DEA 1- 혈액형 긴급 헌혈이 절실해요ㅠㅠ",
+        "created_at": "2024-07-27T00:24:17.494386",
+        "updated_at": "2024-07-27T00:24:17.494386",
+        "image": null,
+        "comments_cnt": 0,
+        "like_num": 0,
+        "blood": "DEA 1-",
+        "region": "서울"
+    }
+]
+
+export default dummyBloodPost;
