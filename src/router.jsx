5973b1079976875155a6ebffc2c533eef870c628
2021110765@dgu.ac.kr <2021110765@dgu.ac.kr>
2024-07-31 18:07:51 +0900

    save

--- src/router.jsx
+++ src/router.jsx
@@ -6,14 +6,14 @@ import Home from "./pages/homePage/Home";
 import NotFound from "./pages/NotFound/NotFound";
 import BloodPost from "./pages/bloodPostPage/BloodPost";
 import BloodReservation from "./pages/reservationPage/BloodReservation";
-import MyPage from "./pages/myPage/MyPage";
+import MyPage from "./pages/myPage/MyPageMain";
 import MyPageEdit from "./pages/myPage/MyPageEdit";
 import AccountManage from "./pages/myPage/AccountManage";
 import PostWrite from "./pages/postWritePage/PostWrite";
 import SignUp from "./pages/signupPage/SignUp";
 import SignUpPet from "./pages/signupPage/SignUpPet";
 
-import BloodPostDetail from './pages/bloodPostDetailPage/BloodPostDetail';
+import BloodPostDetail from "./pages/bloodPostDetailPage/BloodPostDetail";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -23,14 +23,14 @@ const router = createBrowserRouter([
       { path: "/login", element: <Login /> }, // 로그인 화면
       { path: "/home", element: <Home /> }, // 홈 화면
       { path: "/bloodPost", element: <BloodPost /> }, // 긴급헐혈 화면
-      { path: "/bloodPost/:id", element: <BloodPostDetail /> },//긴급헌혈 상세게시글 들어가기
+      { path: "/bloodPost/:id", element: <BloodPostDetail /> }, //긴급헌혈 상세게시글 들어가기
       { path: "/reservation", element: <BloodReservation /> }, // 헌열예약 화면
-      { path: "/mypage", element: <MyPage /> }, // 마이페이지 화면
+      { path: "/mypage", element: <MyPageMain /> }, // 마이페이지 화면
       { path: "/mypageedit", element: <MyPageEdit /> }, // 마이페이지 프로필 수정 화면
       { path: "/accountmanage", element: <AccountManage /> }, // 마이페이지 계정 관리 화면
       { path: "/postwrite", element: <PostWrite /> }, // 긴급헌혈에서 글쓰기 화면
-      { path: "/signup", element: <SignUp /> }, //회원가입 화면 
-      { path: "/signuppet", element: <SignUpPet /> }
+      { path: "/signup", element: <SignUp /> }, //회원가입 화면
+      { path: "/signuppet", element: <SignUpPet /> },
     ],
     errorElement: <NotFound />, // 일치하는 경로가 없을 때 표시할 컴포넌트
   },
