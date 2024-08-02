c5d24122bb00cff8a6a44146dce5cee9367ba935
nicerjs23 <nicerjs23@gmail.com>
2024-08-02 19:44:37 +0900

    ✨ Feat: 전체가로길이 600제한+ 홈화면 디자인중

--- src/pages/homePage/Styled.jsx
+++ src/pages/homePage/Styled.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import BgImg from '../../assets/images/HomeBG.svg'
-
+import PetIcon from '../../assets/icons/PetIcon.svg'
 export const Wrapper = styled.div`
     display  :flex ;
     flex-direction: column;
@@ -29,17 +29,22 @@ export const Bg = styled.div`//배경 이미지
 
 
 export const OverlapContent = styled.div`//배경이미지와 겹치는 헌혈견테스트 부분 
-  position: absolute;
-  top: 65%; // 배경 이미지와 겹치는 부분의 위치 조정
-  width: 90%;
+    position: absolute;
+    top: 65%; // 배경 이미지와 겹치는 부분의 위치 조정
+    width: 90%;
 
-  height: 172px;
-  background-color: white;
-  padding: 20px;
-  border-radius: 20px;
+    height: 172px;
+    background-color: white;
+    padding: 20px;
+    border-radius: 20px;
   box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1); // 그림자 효과
 `;
 
+export const PetTest = styled.div`
+    display: flex;
+    width: 90%;
+    justify-content: space-between;
+`;
 export const ContentWrapper=styled.div`
     display: flex;
     width: 90%;
