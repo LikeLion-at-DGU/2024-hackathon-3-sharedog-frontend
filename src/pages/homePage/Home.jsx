f7f47503af377fcfa59d7b978a857a04de4b48b9
nicerjs23 <nicerjs23@gmail.com>
2024-08-03 16:22:48 +0900

    ✨  Feat: 프로젝트명 변경, 슬라이드 디자인,로그인함수추가

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -28,7 +28,6 @@ const Home = () => {
     return (
         
     <Wrapper>
-
         <Bg>
             <Header/>
             <PetTestCard/>
--- src/pages/homePage/Slider.jsx
+++ src/pages/homePage/Slider.jsx
@@ -13,42 +13,53 @@ const SampleSlider = styled.div`
   position: relative;
 
   .swiper-pagination {
+
     position: absolute;
-    bottom: 10px; /* 페이지네이션 점(dot)을 슬라이더 아래로 이동 */
+    bottom: 3px; /* 페이지네이션 점(dot)을 슬라이더 아래로 이동 */
     left: 0;
     width: 100%;
     display: flex;
     justify-content: center;
-  }
 
+  }
+.swiper-pagination-bullet{
+  background-color: #FF6969;
+}
   .swiper-button-prev,
   .swiper-button-next {
-    color: blue;
-    width: 40px; /* 버튼 크기 키우기 */
-    height: 40px; /* 버튼 크기 키우기 */
+
+    color: #FF6969;
+    width: 33px; /* 버튼 크기 키우기 */
+    height: 33px; /* 버튼 크기 키우기 */
+    border-radius: 78.571px;
+    border: 0.786px solid var(--Red-Red01, rgba(255, 105, 105, 0.40));
+    background: #FFF;
+    box-shadow: 0px 0px 10px 0px rgba(255, 36, 74, 0.10);
     &:after {
-      font-size: 20px; /* 아이콘 크기 키우기 */
+      font-size: 15px; /* 아이콘 크기 키우기 */
     }
   }
 `;
 
 const Slide = styled.div`
   width: 100%;
-  background-color: ${(props) => props.bgColor};
-  color: white;
+  border-radius: 10px;
+  border: 1px solid var(--Color-Gray-Gray01, #EAEAEA);
+  background: #FFF;
+  color: black;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 24px;
-  height: 200px; /* 슬라이더 높이 설정 */
+  height: 90px; /* 슬라이더 높이 설정 */
 `;
 
 const Slider = () => {
   const slides = [
-    { id: 1, bgColor: 'red' },
-    { id: 2, bgColor: 'blue' },
-    { id: 3, bgColor: 'green' },
-    { id: 4, bgColor: 'pink' },
+    { id: 1},
+    { id: 2},
+    { id: 3 },
+    { id: 4 },
   ]; // 슬라이드에 표시할 숫자와 색상
 
   return (
@@ -59,9 +70,9 @@ const Slider = () => {
         slidesPerView={1.5} // 슬라이드가 중간에 정렬되고 양옆이 반씩 보이도록 설정
         centeredSlides={true} // 가운데 정렬
         spaceBetween={20}
-        autoplay={{
-          delay: 2000,
-        }}
+        // autoplay={{
+        //   delay: 2000,
+        // }}
         pagination={{
           clickable: true,
         }}
@@ -69,7 +80,7 @@ const Slider = () => {
       >
         {slides.map((slide) => (
           <SwiperSlide key={slide.id}>
-            <Slide bgColor={slide.bgColor}>sample{slide.id}</Slide>
+            <Slide >sample{slide.id}</Slide>
           </SwiperSlide>
         ))}
       </Swiper>
