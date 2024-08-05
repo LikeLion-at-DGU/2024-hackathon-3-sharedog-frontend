dbe6fef736dedd5d1fa082a98964335c49b87619
pedro0527 <sooyong0527@naver.com>
2024-08-05 21:49:36 +0900

    Feat : 테스트 페이지 초기 세팅 완료 및 첫페이지 구현 완료

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,12 @@ const Layout = () => {
   const location = useLocation();
   const hideHeaderFooter = location.pathname === '/' 
         || location.pathname === '/login' || location.pathname === '/signupsplash' 
-        || location.pathname === '/signup' || location.pathname === '/SignUpPet';
+        || location.pathname === '/signup' || location.pathname === '/SignUpPet'
+        || location.pathname === '/testFirst';
+        // || location.pathname === '/testSecond'
+        // || location.pathname === '/testThird' || location.pathname === '/testFourth'
+        // || location.pathname === '/testFifth' || location.pathname === '/testSixth'
+        // || location.pathname === '/testFinal';
 
   return (
     <Wrapper>
