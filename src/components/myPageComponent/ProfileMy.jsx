979ad06e7e1f0f82f834f96ba64500d8e939b2ec
2021110765@dgu.ac.kr <2021110765@dgu.ac.kr>
2024-08-05 21:32:57 +0900

    fix: 디자인 컴포넌트에 api 연결한거 삭제

--- src/components/myPageComponent/ProfileMy.jsx
+++ src/components/myPageComponent/ProfileMy.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import EditMyPageSVG from "../../assets/icons/editMyPage.svg?react";
-import ProfileMyPageSVG from "../../assets/icons/profileMyPage.svg?react";
-import { useNavigate } from "react-router-dom";
-import { API } from "../../api"; // API import
+// Styled.js 파일에서
+import styled, { css } from "styled-components";
+import EditMyPageSVG from "../../assets/icons/editMyPage.svg?react"; // SVG 파일 가져오기
+import ProfileMyPageSVG from "../../assets/icons/profileMyPage.svg?react"; // SVG 파일 가져오기
+import PictureMyPageSVG from "../../assets/icons/pictureMyPage.svg?react"; // SVG 파일 가져오기
+import { useNavigate } from "react-router-dom"; // useNavigate 훅 가져오기
 
 export const Wrapper = styled.div`
   display: flex;
-  align-items: center;
-  flex-direction: column;
+  align-items: center; /* 수평 중앙 정렬 */
+  flex-direction: column; /* 요소들을 수직 정렬 */
   width: 100%;
   font-size: 20px;
   font-family: SUIT, sans-serif;
   font-weight: 800;
-  padding-top: 10px;
+  padding-top: 10px; /* 상단 여백 추가 */
 `;
 
 export const ProfileContainer = styled.div`
@@ -25,15 +25,17 @@ export const ProfileContainer = styled.div`
   border: solid 1px rgba(234, 234, 234, 0.8);
   overflow: hidden;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
   justify-content: center;
   align-items: center;
   flex-shrink: 0;
+
   color: var(--Grayscale-Gray700, #222b39);
   font-family: SUIT;
   font-size: 16px;
   font-style: normal;
   font-weight: 700;
-  line-height: 160%;
+  line-height: 160%; /* 28.8px */
   letter-spacing: -0.36px;
 `;
 
@@ -46,10 +48,14 @@ export const EditBtn = styled.button`
   border: solid 1px rgba(234, 234, 234, 0.8);
   overflow: hidden;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
   justify-content: center;
   align-items: center;
   flex-shrink: 0;
+
+  border-radius: 0px 0px 12px 12px;
   background: rgba(255, 105, 105, 0.15);
+
   color: var(--Red-Red04, #ff6969);
   font-family: SUIT;
   font-size: 12px;
@@ -61,49 +67,40 @@ export const EditBtn = styled.button`
 
 export const ProfileImg = styled.div`
   padding: 15.058px 15.83px;
+
   border-radius: 32.047px;
   border-style: none;
   background: #eaeaec;
 `;
 
+export const PictureImg = styled.div`
+  width: 20px;
+  height: 15px;
+  flex-shrink: 0;
+`;
+
 const ProfileMy = () => {
-  const [profiles, setProfiles] = useState([]);
-
-  const fetchData = async () => {
-    try {
-      const response = await API.get("/api/users/myprofile");
-      console.log("진우데이터:", response.data);
-      setProfiles(response.data); // 데이터 배열로 설정
-    } catch (error) {
-      console.error("에러입니당:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // useNavigate 훅 초기화
 
   const handleEditClick = () => {
-    navigate("/MyPageEdit");
+    navigate("/MyPageEdit"); // /mypageedit 경로로 이동
   };
-
   return (
-    <Wrapper>
-      {profiles.map((profile, index) => (
-        <ProfileContainer key={index}>
+    <>
+      <Wrapper>
+        <ProfileContainer>
           <ProfileImg>
             <ProfileMyPageSVG />
           </ProfileImg>
-          {profile.nickname}
+          황민영
         </ProfileContainer>
-      ))}
-      <EditBtn onClick={handleEditClick}>
-        <EditMyPageSVG /> 프로필 수정
-      </EditBtn>
-    </Wrapper>
+        <EditBtn onClick={handleEditClick}>
+          <EditMyPageSVG /> 프로필 수정
+        </EditBtn>
+      </Wrapper>
+    </>
   );
 };
 
 export default ProfileMy;
+export { EditMyPageSVG, ProfileMyPageSVG, PictureMyPageSVG };
